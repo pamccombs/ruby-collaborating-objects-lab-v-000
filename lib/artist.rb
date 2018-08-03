d3567017fe46276b02eb72ac7e132a141b4e7d04
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-03 23:04:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,12 +24,6 @@ class Artist
     
   end
   
-  def self.find_by_name(name)
-    
-    self.all.find {|artist| artist.name == name}
-    
-  end
-  
   def self.create_by_name(name)
     
     artist = self.new(name)
@@ -38,13 +32,17 @@ class Artist
     
   end
   
+  def self.find_by_name(name)
+    
+    self.all.find {|artist| artist.name == name}
+    
+  end
+  
   def self.find_or_create_by_name(name)
     
     self.find_by_name(name) || self.create_by_name(name)
     
   end
-  
-  
   
   def add_song(song)
     
