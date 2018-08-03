613a8a47acd00c7a902ba9acc2c4c544f67d86f2
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-03 23:01:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -37,19 +37,25 @@ class Artist
     
   end
   
-   def save
+  def save
+     
     @@all << self
+    
   end
-   def add_song(song)
+  
+  def add_song(song)
+    
     @songs << song
+    
     song.artist = self
+    
   end
    
-   
-   
-   def print_songs
+  def print_songs
+    
     self.songs.each do |song|
       puts song.name
+      
     end
   end
 end
